483f33d8d337202c31e82aabbaeea1e8d03da645
samtkaplan <sam.kaplan@chevron.com>
2018-05-09 15:00:06 -0500

    generalize to any Julia type

--- src/ParallelOperations.jl
+++ src/ParallelOperations.jl
@@ -2,22 +2,24 @@ __precompile__(true)
 
 module ParallelOperations
 
-struct ArrayFutures{T,N} f::Dict{Int,Future} end
-Base.getindex(a::ArrayFutures, i::Int) = getindex(a.f, i)
-Base.setindex!(a::ArrayFutures, f::Future, i::Int) = setindex!(a.f, f, i)
-Base.keys(a::ArrayFutures) = keys(a.f)
+struct TypeFutures{T} f::Dict{Int,Future} end
+Base.getindex(a::TypeFutures, i::Int) = getindex(a.f, i)
+Base.setindex!(a::TypeFutures, f::Future, i::Int) = setindex!(a.f, f, i)
+Base.keys(a::TypeFutures) = keys(a.f)
 
-function ArrayFutures(_T::Type{T}, n::NTuple{N,Int}, pids=procs()) where {T,N}
+function TypeFutures(_T::Type{T}, f::Function, pids::AbstractArray, fargs::Vararg) where {T}
     pids[1] == myid() || error("expected myid()==pids[1], got pids[1]=$(pids[1]) where-as myid()=$(myid())")
 
     futures = Dict()
     @sync for pid in pids
-        futures[pid] = remotecall(zeros, pid, T, n)
+        futures[pid] = remotecall(f, pid, fargs...)
         @async remotecall_fetch(wait, pid, futures[pid])
     end
-    ArrayFutures{T,N}(futures)
+    TypeFutures{T}(futures)
 end
-function ArrayFutures(x::Array{T,N}, pids=procs()) where {T,N}
+TypeFutures(_T::Type{T}, f::Function, fargs::Vararg) where {T} = TypeFutures(_T, f, procs(), fargs...)
+
+function TypeFutures(x::T, f::Function, pids::AbstractArray, fargs::Vararg) where {T}
     futures = Dict()
     n = size(x)
     @sync for pid in pids
@@ -25,14 +27,19 @@ function ArrayFutures(x::Array{T,N}, pids=procs()) where {T,N}
             futures[pid] = Future()
             put!(futures[pid], x)
         else
-            futures[pid] = remotecall(zeros, pid, T, n)
+            futures[pid] = remotecall(f, pid, fargs...)
             @async remotecall_fetch(wait, pid, futures[pid])
         end
     end
-    ArrayFutures{T,N}(futures)
+    TypeFutures{T}(futures)
 end
+TypeFutures(x::T, f::Function, fargs::Vararg) where {T} = TypeFutures(x, f, pids, fargs...)
 
-function bcast(x::AbstractArray{T,N}, pids=procs()) where {T,N}
+ArrayFutures{T,N} = TypeFutures{Array{T,N}}
+ArrayFutures(_T::Type{T}, n::NTuple{N,Int}, pids=procs()) where {T,N} = TypeFutures(Array{T,N}, zeros, pids, T, n)
+ArrayFutures(x::Array{T,N}, pids=procs()) where {T,N} = TypeFutures(x, zeros, pids, T, size(x))
+
+function bcast(x::T, pids=procs()) where {T}
     pids[1] == myid() || error("expected myid()==pids[1], got pids[1]=$(pids[1]) where-as myid()=$(myid())")
 
     M = length(pids)
@@ -58,14 +65,15 @@ function bcast(x::AbstractArray{T,N}, pids=procs()) where {T,N}
         end
     end
 
-    ArrayFutures{T,N}(futures)
+    TypeFutures{T}(futures)
 end
 
-function reduce!(futures::ArrayFutures{T,N}) where {T,N}
-    function _reduce!(future_mine, future_theirs, T::DataType, N::Int)
-        x = remotecall_fetch(fetch, future_theirs.where, future_theirs)::Array{T,N}
-        y = fetch(future_mine)::Array{T,N}
-        y .+= x
+@inline paralleloperations_reduce!(y, x) = (y .+= x)
+function reduce!(futures::TypeFutures{T}, reducemethod!::Function=paralleloperations_reduce!) where {T}
+    function _reduce!(future_mine, future_theirs, T::DataType)
+        x = remotecall_fetch(fetch, future_theirs.where, future_theirs)::T
+        y = fetch(future_mine)::T
+        reducemethod!(y, x)
         nothing
     end
 
@@ -77,49 +85,55 @@ function reduce!(futures::ArrayFutures{T,N}) where {T,N}
 
     if R != 0
         @sync for i = 1:R
-            @async remotecall_fetch(_reduce!, pids[i], futures[pids[i]], futures[pids[m+i]], T, N)
+            @async remotecall_fetch(_reduce!, pids[i], futures[pids[i]], futures[pids[m+i]], T)
         end
     end
 
     for l = L:-1:1
         m = 2^(l-1)
         @sync for i = 1:m
-            @async remotecall_fetch(_reduce!, pids[i], futures[pids[i]], futures[pids[m+i]], T, N)
+            @async remotecall_fetch(_reduce!, pids[i], futures[pids[i]], futures[pids[m+i]], T)
         end
     end
-    fetch(futures[myid()])::Array{T,N}
+    fetch(futures[myid()])::T
 end
 
-function Base.copy!(to::ArrayFutures, from::ArrayFutures, pids=procs())
-    function _copy!(future_to, future_from)
+@inline paralleloperations_copy!(x, y) = (x .= y)
+function Base.copy!(to::TypeFutures, from::TypeFutures, copymethod!::Function, pids::AbstractArray=Int[])
+    pids = isempty(pids) ? keys(to) : pids
+    function _copy!(future_to, future_from, copymethod!)
         x = fetch(future_to)
         y = fetch(future_from)
-        x .= y
+        copymethod!(x, y)
         nothing
     end
     @sync for pid in pids
-        @async pid ∈ keys(to) && pid ∈ keys(from) && remotecall_fetch(_copy!, pid, to[pid], from[pid])
+        @async pid ∈ keys(to) && pid ∈ keys(from) && remotecall_fetch(_copy!, pid, to[pid], from[pid], copymethod!)
     end
 end
+Base.copy!(to::TypeFutures, from::TypeFutures, pids::AbstractArray=procs()) = copy!(to, from, paralleloperations_copy!, pids)
 
-function Base.fill!(futures::ArrayFutures, a::Number, pids=Int[])
+@inline paralleloperations_fill!(x, a) = (x .= a)
+function Base.fill!(futures::TypeFutures, a::Number, fillmethod!::Function, pids::AbstractArray=Int[])
     pids = isempty(pids) ? keys(futures) : pids
-    function _fill!(future, a)
+    function _fill!(future, a, fillmethod!)
         x = fetch(future)
-        x .= a
+        fillmethod!(x,a)
         nothing
     end
     @sync for pid in pids
-        @async remotecall_fetch(_fill!, pid, futures[pid], a)
+        @async remotecall_fetch(_fill!, pid, futures[pid], a, fillmethod!)
     end
 end
+Base.fill!(futures::TypeFutures, a::Number, pids::AbstractArray=Int[]) = fill!(futures, a, paralleloperations_fill!, pids)
 
 using DistributedArrays
 import DistributedArrays.localpart
-localpart(futures::ArrayFutures{T,N}) where {T,N} = fetch(futures[myid()])::Array{T,N}
+localpart(futures::TypeFutures{T}) where {T} = fetch(futures[myid()])::T
 
-Base.show(io::IO, futures::ArrayFutures) = write(io, "ArrayFutures with pids=$(keys(futures)) and size $(size(localpart(futures)))")
+Base.show(io::IO, futures::TypeFutures) = write(io, "TypeFutures with pids=$(keys(futures)) and type $(typeof(localpart(futures)))")
+Base.show(io::IO, futures::ArrayFutures) = write(io, "ArrayFutures with pids=$(keys(futures)) and type $(size(localpart(futures)))")
 
-export ArrayFutures, bcast, localpart, reduce!
+export ArrayFutures, TypeFutures, bcast, localpart, reduce!
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ using Base.Test
     end
 end
 
-@testset "Broadcast" for n in ((10,), (10,11)), T in (Float32,Float64)
+@testset "Broadcast, array" for n in ((10,), (10,11)), T in (Float32,Float64)
     x = rand(T,n)
     futures = bcast(x)
     for pid in procs()
@@ -26,7 +26,16 @@ end
     end
 end
 
-@testset "Reduce" for n in ((10,), (10,11)), T in (Float32,Float64)
+@testset "Broadcast, type" for n = ((10,), (10,11)), T in (Float32,Float64)
+    x = (rand(T,n),rand(T,n))
+    futures = bcast(x)
+    for pid in procs()
+        @test fetch(futures[pid])[1] ≈ x[1]
+        @test fetch(futures[pid])[2] ≈ x[2]
+    end
+end
+
+@testset "Reduce, array" for n in ((10,), (10,11)), T in (Float32,Float64)
     futures = ArrayFutures(T, n)
     @everywhere myfill!(future) = begin rand!(fetch(future)); nothing end
     @sync for pid in procs()
@@ -42,7 +51,29 @@ end
     @test reduce!(futures) ≈ expected
 end
 
-@testset "localpart" for n in ((10,), (10,11)), T in (Float32,Float64)
+@testset "Reduce, type" for n = ((10,), (10,11)), T in (Float32,Float64)
+    _foo() = (zeros(T,n),zeros(T,n))
+    N = length(n)
+    futures = TypeFutures(Tuple{Array{T,N},Array{T,N}}, _foo)
+    @everywhere myfill!(future) = begin t = fetch(future); rand!(t[1]); rand!(t[2]); nothing end
+    @sync for pid in procs()
+        @async remotecall_fetch(myfill!, pid, futures[pid])
+    end
+    expected = (zeros(T,n),zeros(T,n))
+    for pid in procs()
+        x = remotecall_fetch(fetch, pid, futures[pid])
+        expected[1] .+= x[1]
+        expected[2] .+= x[2]
+    end
+    for pid in procs()
+        fetch(futures[pid])
+    end
+    observed = reduce!(futures, (y,x)->begin y[1] .+= x[1]; y[2] .+= x[2] end)
+    @test observed[1] ≈ expected[1]
+    @test observed[2] ≈ expected[2]
+end
+
+@testset "localpart, array" for n in ((10,), (10,11)), T in (Float32,Float64)
     futures = ArrayFutures(T, n)
     @everywhere myfill!(future) = begin fill!(fetch(future), myid()*π); nothing end
     @sync for pid in procs()
@@ -53,7 +84,18 @@ end
     end
 end
 
-@testset "copy!" for n in ((10,), (10,11)), T in (Float32,Float64)
+@testset "localpart, type" for n = ((10,), (10,11)), T in (Float32,Float64)
+    N = length(n)
+    _foo() = (myid()*T(π)*ones(T,n), 2*myid()*T(π)*ones(T,n))
+    futures = TypeFutures(Tuple{Array{T,N},Array{T,N}}, _foo)
+    for pid in procs()
+        r = remotecall_fetch(localpart, pid, futures)
+        @test r[1] ≈ pid*π*ones(T,n)
+        @test r[2] ≈ 2*pid*π*ones(T,n)
+    end
+end
+
+@testset "copy!, array" for n in ((10,), (10,11)), T in (Float32,Float64)
     futures = ArrayFutures(T,n)
     @everywhere myfill!(future) = begin fill!(fetch(future), myid()*π); nothing end
     @sync for pid in procs()
@@ -75,7 +117,32 @@ end
     end
 end
 
-@testset "fill!" for n in ((10,), (10,11)), T in (Float32,Float64)
+@testset "copy!, type" for n in ((10,), (10,11)), T in (Float32,Float64)
+    N = length(n)
+    _foo() = (myid()*T(π)*ones(T,n), 2*myid()*T(π)*ones(T,n))
+    futures = TypeFutures(Tuple{Array{T,N},Array{T,N}}, _foo)
+    futures_copy = TypeFutures(Tuple{Array{T,N},Array{T,N}}, ()->(zeros(T,n),zeros(T,n)))
+    copy!(futures_copy, futures, (x,y)->begin x[1] .=  y[1]; x[2] .= y[2] end)
+    for pid in procs()
+        r = remotecall_fetch(localpart, pid, futures_copy)
+        @test r[1] ≈ pid*π*ones(T,n)
+        @test r[2] ≈ 2*pid*π*ones(T,n)
+    end
+    fill!(futures_copy, 0, (x,a)->begin x[1] .= a; x[2] .= a end)
+    copy!(futures_copy, futures, (x,y)->begin x[1] .= y[1]; x[2] .= y[2] end, [3,5])
+    for pid in procs()
+        r = remotecall_fetch(localpart, pid, futures_copy)
+        if pid ∈ (3,5)
+            @test r[1] ≈ pid*π*ones(T,n)
+            @test r[2] ≈ 2*pid*π*ones(T,n)
+        else
+            @test r[1] ≈ zeros(T,n)
+            @test r[2] ≈ zeros(T,n)
+        end
+    end
+end
+
+@testset "fill!, array" for n in ((10,), (10,11)), T in (Float32,Float64)
     futures = ArrayFutures(T,n)
     fill!(futures, π)
     for pid in procs()
@@ -87,6 +154,23 @@ end
             @test remotecall_fetch(localpart, pid, futures) ≈ 2*π*ones(T,n)
         else
             @test remotecall_fetch(localpart, pid, futures) ≈ π*ones(T,n)
+        end
+    end
+end
+
+@testset "fill!, type" for n in ((10,), (10,11)), T in (Float32,Float64)
+    N = length(n)
+    _foo() = (T(π)*ones(T,n), 2*T(π)*ones(T,n))
+    futures = TypeFutures(Tuple{Array{T,N},Array{T,N}}, _foo)
+    fill!(futures, 3*π, (x,a)->begin x[1] .= a; x[2] .= a end, [3,5])
+    for pid in procs()
+        r = remotecall_fetch(localpart, pid, futures)
+        if pid ∈ (3,5)
+            @test r[1] ≈ 3*π*ones(T,n)
+            @test r[2] ≈ 3*π*ones(T,n)
+        else
+            @test r[1] ≈ π*ones(T,n)
+            @test r[2] ≈ 2*π*ones(T,n)
         end
     end
 end
